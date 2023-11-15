842a8ae60ba5177f1fcfdcb8b1947b93ffda63f9
Mateus Daniel de Lima da Silva <81442147+mateusdanie@users.noreply.github.com>
2023-11-15 09:40:41 -0300

    Feature - Adjusting logic

--- include/main.h
+++ include/main.h
@@ -1,7 +1,7 @@
 #include <libs.h>
 #include <entities.h>
 
-void readGraph(State *Start, char **InputAlphabet, char **Ribbonalphabet, char *FilePath)
+void readGraph(State *Start, char *InputAlphabet, char *Ribbonalphabet, char *FilePath)
 {
     FILE *file;
     file = fopen(FilePath, "r");
@@ -25,44 +25,59 @@ void readGraph(State *Start, char **InputAlphabet, char **Ribbonalphabet, char *
     {   
         Aux = NULL;
         content_sliced = strtok(content, " ");
-        if(strcmp(content_sliced,"E") == 0){
-            while(content_sliced != NULL){
+
+        if(strcmp(content_sliced,"E") == 0)
+        {
+            while(content_sliced != NULL)
+            {
                 content_sliced = strtok(NULL, slice_delimitatator);
-                if(i == 0 && content_sliced != NULL){
-                    struct State* New_State = (struct State *)malloc(sizeof(struct State));
-                    New_State->Identifier = content_sliced;
-                    Start_List = New_State;
-                }else if (i == 1 && content_sliced != NULL)
+
+                if(i == 0 && content_sliced != NULL)
                 {
                     struct State* New_State = (struct State *)malloc(sizeof(struct State));
                     New_State->Identifier = content_sliced;
+                    New_State->Status = "Transition";
+                    Start_List = New_State;
+                }
+                else if (i == 1 && content_sliced != NULL)
+                {
+                    struct State* New_State = (struct State *)malloc(sizeof(struct State));
+                    New_State->Identifier = content_sliced;
+                    New_State->Status = "Transition";
                     Start_List->Next_State = New_State;
                     Aux = New_State;
-                }else if(i > 1 && content_sliced != NULL)
+                }
+                else if(i > 1 && content_sliced != NULL)
                 {
                     struct State* New_State = (struct State *)malloc(sizeof(struct State));
                     New_State->Identifier = content_sliced;
+                    New_State->Status = "Transition";
                     Aux->Next_State = New_State;   
                     Aux = Aux->Next_State;
                 }
 
                 i++;
             }
-        }else if(strcmp(content_sliced,"A") == 0)
+        }
+        else if(strcmp(content_sliced,"A") == 0)
         {   
-            while(content_sliced != NULL){
+            while(content_sliced != NULL)
+            {
                 content_sliced = strtok(NULL, slice_delimitatator);
                 if(content_sliced  != NULL)
-                    strcat(*InputAlphabet, content_sliced);
+                    strcat(InputAlphabet, content_sliced);
             }
-        }else if(strcmp(content_sliced,"F") == 0)
+        }
+        else if(strcmp(content_sliced,"F") == 0)
         {   
-            while(content_sliced != NULL){
+            while(content_sliced != NULL)
+            {
                 content_sliced = strtok(NULL, slice_delimitatator);
                 if(content_sliced  != NULL)
-                    strcat(*Ribbonalphabet, content_sliced);
+                    strcat(Ribbonalphabet, content_sliced);
             }   
-        }else if(strcmp(content_sliced,"D") == 0)
+        }
+        else if(strcmp(content_sliced,"D") == 0)
         {   
             Aux = Start_List;
             j = 0;
@@ -70,19 +85,23 @@ void readGraph(State *Start, char **InputAlphabet, char **Ribbonalphabet, char *
             content_sliced = strtok(NULL, slice_delimitatator);
             while (Aux->Next_State != NULL)
             {
-                if(strcmp(Aux->Identifier, content_sliced) == 0){
+                if(strcmp(Aux->Identifier, content_sliced) == 0)
+                {
                     break;
-                }else{
+                }
+                else
+                {
                     Aux = Aux->Next_State;
                 }
             }
             
-        }else if (strcmp(content_sliced,"S") == 0)
+        }
+        else if (strcmp(content_sliced,"S") == 0)
         {
             content_sliced = strtok(NULL, slice_delimitatator);
-            Aux->Status = (char *) malloc(sizeof(content_sliced));
             Aux->Status = content_sliced;
-        }else if (strcmp(content_sliced,"T") == 0)
+        }
+        else if (strcmp(content_sliced,"T") == 0)
         {
             Aux2 = Start_List;
             while(content_sliced != NULL){
@@ -92,7 +111,7 @@ void readGraph(State *Start, char **InputAlphabet, char **Ribbonalphabet, char *
                 else if (j == 1 && content_sliced  != NULL)
                     Aux->TransitionsFunctions[i].Write = content_sliced;
                 else if (j == 2 && content_sliced  != NULL)
-                    Aux->TransitionsFunctions[i].HeadMovement =content_sliced;
+                    Aux->TransitionsFunctions[i].HeadMovement = content_sliced;
                 else if (j == 3 && content_sliced  != NULL)
                     while (Aux2->Next_State != NULL)
                     {
--- source/main.c
+++ source/main.c
@@ -1,18 +1,20 @@
-#include <main.h>
+#include "../include/main.h"
+
+//aAbB 
 
 State *Start = NULL;
-char **InputAlphabet = NULL; // Equivalente a uma lista de strings ou matrix de caracteres
-char **Ribbonalphabet = NULL; // Equivalente a uma lista de strings ou matrix de caracteres
+char *InputAlphabet = NULL; // Equivalente a uma lista de strings ou matrix de caracteres
+char *Ribbonalphabet = NULL; // Equivalente a uma lista de strings ou matrix de caracteres
 
 int main(int argc, char * argv[])
 {
-    if(argc < 2 || argc > 2)
+    /*if(argc < 2 || argc > 2)
     {
         printf("[Aviso]: Numero invalido de parametros !\n[Info]: Siga o exemplo: TuringMachine.exe caminho/para/arquivo.extensão");
         return EXIT_FAILURE;
-    }
+    }*/
 
-    readGraph(Start, InputAlphabet, Ribbonalphabet, argv[0]);
+    readGraph(Start, &InputAlphabet, &Ribbonalphabet, "../TuringMachine.txt"  /*argv[0]*/);
 
     return EXIT_SUCCESS;
 }
